67a4468caf4189275587028fc429499860cfa625
PlakaSorgu <1muharremcandan@gmail.com>
2021-01-31 02:02:22 +0300

    Add project files.

---
+++ Program.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace GenericsIntro
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            List<int> users = new List<int>();
+            MyDictionary<string> isimler = new MyDictionary<string>();
+             
+            
+            while (true)
+            {
+                Console.Write("Please enter a name.(Press to q to finish.!)\nName:");
+                string name = Console.ReadLine();
+                if (name.ToUpper().Equals("Q"))
+                {
+                    Console.WriteLine("System shutting down...");
+                    break;
+                }
+                isimler.Add(name);
+
+            }
+            isimler.ShowMyList();
+        }
+    }
+}
